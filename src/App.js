4bf772533395b2a5bbe3031ebe066dc8c2749923
Maldrek <scott.salem5@gmail.com>
2018-06-27 21:50:58 -0500

    moved files over from my folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Landing from './pages/Landing';
+import './App.css';
+import 'tachyons';
+
+class App extends Component {
+  render() {
+    return < Landing />
+  }
+}
+
+export default App;
---
+++ src/pages/Landing.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Jumbo1 from '../landingComponents/Jumbotron1';
+import NavBar from '../landingComponents/NavBar';
+import Parallax1 from '../landingComponents/Parallax1';
+import Parallax2 from '../landingComponents/Parallax2';
+import Info from '../landingComponents/Info1';
+import Jumbo2 from '../landingComponents/Jumbotron2';
+import '../App.css';
+import 'tachyons';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Jumbo1 />
+        <Parallax1 />
+        <Info />
+        <Parallax2 />
+        <Jumbo2 />
+      </div>
+    );
+  }
+}
+
+export default Landing;
